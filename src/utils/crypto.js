53bded06648c98b6d61d8c0adc26b0966981df46
Yunyang <sunnycloudyang@outlook.com>
2024-12-26 02:37:28 +0800

    fix: update key pair

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,15 +1,15 @@
 import JSEncrypt from 'jsencrypt';
 
 // Public key should be injected via environment variable
-const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY || `
+const PUBLIC_KEY = `
 -----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw35WyKqwE+yGZvBrQUai
-kpDcIQEwYyW+GG0H5JfP8vTPTzqvC1jOc9PCJaBbCEYQ7I0jpQMg80rLqO/4Pkyn
-w9kbQEM+TCAgVfARdmPZR8w+nzzM08/xDSR6aYWSip3SNO5jXph3QgbnEL+m50Zc
-NL2d1FQFtADsO06CRNXKXS2VzPcoJRgtjVe8VPR5nnivBLKcHyKEpyIswZ4AMd3n
-rQ2+ux86LF07J2VjTSqOAsnPqLXRtUd6HEY9Em43Qa2wYOpNCbeobPI9A/pJXo9n
-0w+uCqP0322YNkEdGeXHBLuJDgcfeUmRF23ZQKbnZA2WXPwwrVgyRogw6jZj1Slv
-lQIDAQAB
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwHSvtdY4mxujKyRd6KS9
+IMmXscqW3ftrjGdH1LA+/SJOhlgnCL8YcEvRJ3u8dtwvbU3n/kOgFJqwqXWEc5gY
+/dIs/qe15r4ecHW8KoqwnJbp2njehEPcvGt5ONfUU2SURzuVnStQIuXceOwSLlqm
+qHvseTNEq+yYuemqr7DkjtJgevJ5p+sGBSawAJrRPYbs1I1ZET2WaPXxf2N14v6d
+5kkKh5IikFPXNDVHjPCOZoEi17qEbh2fJqRbf8Nx0HA2ScjAizpDLbYTroWgr7vZ
+8iIUGxFLSmgNUopeNycfM6sIRqh0mM78CzImiw/WyHiFIiKnUhoXJU0eN6nro42W
+DwIDAQAB
 -----END PUBLIC KEY-----`;
 
 const encryptor = new JSEncrypt();
